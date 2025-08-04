fd759a014b58dac47033ae7883377ad90789d11b
chaehwanli <chaehwan.li@gmail.com>
2025-08-04 23:10:55 +0900

    remove assert condition

--- tests/test_server.py
+++ tests/test_server.py
@@ -101,7 +101,7 @@ def test_status_endpoint_with_some_files_not_analyzed(server):
     data = response.json()
     assert data["version"] == __version__
     data = normalize_version(data)
-    if os.name != 'nt':
+    #if os.name != 'nt':
         assert data["stats"]["chunks"]["unanalyzed"] > 0
     assert data["stats"]["queue"]["size"] >= data["stats"]["chunks"]["unanalyzed"]
     assert data["stats"]["accuracy"]["percentage"] == int(
