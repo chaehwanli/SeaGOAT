49ea12fa5ddba805f0efdbf32c45ed930a1309dc
chaehwanli <chaehwan.li@gmail.com>
2025-08-06 00:06:36 +0900

    revert

--- tests/test_server.py
+++ tests/test_server.py
@@ -102,12 +102,12 @@ def test_status_endpoint_with_some_files_not_analyzed(server):
     assert data["version"] == __version__
     data = normalize_version(data)
     #if os.name != 'nt':
-    #    assert data["stats"]["chunks"]["unanalyzed"] > 0
+    assert data["stats"]["chunks"]["unanalyzed"] > 0
     assert data["stats"]["queue"]["size"] >= data["stats"]["chunks"]["unanalyzed"]
     assert data["stats"]["accuracy"]["percentage"] == int(
         data["stats"]["accuracy"]["percentage"]
     )
-    #assert 0 < data["stats"]["accuracy"]["percentage"] < 100
+    assert 0 < data["stats"]["accuracy"]["percentage"] < 100
 
 
 def test_status_1(repo, runner):
